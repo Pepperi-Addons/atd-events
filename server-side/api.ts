76b46113e411ffdbd8a67a4e3932a37cd127c901
OdedAnswer <89458041+OdedAnswer@users.noreply.github.com>
2024-05-22 15:12:07 +0300

    Di 27277 support atd import export (#38)
    * DI-27277 import/export methods
    
    * added relations and changed upgrade update

--- server-side/api.ts
+++ server-side/api.ts
@@ -3,6 +3,7 @@ import { Client, Request } from '@pepperi-addons/debug-server'
 import { EventsService } from './services/events-service'
 import { ConfigurationsService } from './services/configurations-service';
 import { Draft } from '@pepperi-addons/papi-sdk';
+import { TransactionsService } from './services/transactions-service';
 
 
 export async function get_transactions_events(client: Client, request: Request) {
@@ -49,6 +50,41 @@ export async function get_events(client: Client, request: Request) {
                 throw new Error('draft_key is a required query parameter for this endpoint.');
             }
             return await configurationsService.getATDEvents(draftKey);
+        }
+        default: {
+            throw new Error(`${request.method} not supported`);
+        }
+    }
+}
+
+export async function atd_export(client: Client, request: Request) {
+    switch (request.method) {
+        case 'GET': {
+            const internalID = request.query.internal_id;
+            const transactionService: TransactionsService = new TransactionsService(client, ''); // we don't have the ATD UUID yet and it is not required for this operation
+            const atdUUID = await transactionService.getUUID(parseInt(internalID));
+            const configurationsService: ConfigurationsService = new ConfigurationsService(client);
+            const draft = await configurationsService.getDraft(atdUUID);
+            return { DataForImport: { atdFlowsDraftObject: draft } };
+        }
+        default: {
+            throw new Error(`${request.method} not supported`);
+        }
+    }
+}
+
+export async function atd_import(client: Client, request: Request) {
+    switch (request.method) {
+        case 'POST': {
+            const internalID = request.body.InternalID;
+            const dataFromExport = request.body.DataFromExport;
+            const transactionService: TransactionsService = new TransactionsService(client, ''); // we don't have the ATD UUID yet and it is not required for this operation
+            const atdUUID = await transactionService.getUUID(parseInt(internalID));
+            const configurationsService: ConfigurationsService = new ConfigurationsService(client);
+            const draft = dataFromExport.atdFlowsDraftObject;
+            // need to change the draft key to the ATD UUID
+            draft.Key = atdUUID;
+            return await configurationsService.upsertAndPublishDraft(draft);
         }
         default: {
             throw new Error(`${request.method} not supported`);
