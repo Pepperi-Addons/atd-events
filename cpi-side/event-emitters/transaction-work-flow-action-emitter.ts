50dadb1075cc126c1c16eec8ef79eeb38d02ae0a
Oded Answer <oded.t@pepperi.com>
2024-06-03 11:43:29 +0300

    work in progress

---
+++ cpi-side/event-emitters/transaction-work-flow-action-emitter.ts
@@ -0,0 +1,23 @@
+import { EventsNames } from "shared";
+import { InterceptorData } from "../metadata";
+import { IEventEmitter } from "./event-emitter";
+import { DataObject } from "@pepperi-addons/cpi-node/build/cpi-side/app/entities";
+
+export class TransactionWorkFlowActionEmitter extends IEventEmitter {
+    eventKey: string;
+    constructor(params: InterceptorData, customEventKey: string) {
+        super(params);
+        this.eventKey = customEventKey;
+    }
+
+    protected async getEventData(dataObj: DataObject): Promise<any> {
+        return {
+            TransactionUUID: (dataObj as any).ObjectKey,
+            ObjectType: (dataObj as any).ObjectType
+        }
+    }
+
+    protected getEventKey(): EventsNames {
+        return this.eventKey as EventsNames;
+    }
+}
--- cpi-side/services/subscription-service.ts
+++ cpi-side/services/subscription-service.ts
@@ -1,16 +1,38 @@
 import '@pepperi-addons/cpi-node'
 import { EventEmitterFactory } from '../event-emitters/event-emitter-factory';
 import { InterceptorData } from '../metadata';
+import { AddonUUID } from '../../addon.config.json'
+import { atdFlowsConfigurationSchemaName } from 'shared';
 
 export class SubscriptionService {
     constructor() { }
 
     async registerEvents() {
         pepperi.events.intercept('PreLoadTransactionScope', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'PreLoadTransactionScope') });
-        pepperi.events.intercept('OnLoadTransactionScope', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'OnLoadTransactionScope') });
+        pepperi.events.intercept('OnLoadTransactionScope', {}, async (data, next, main) => {return await this.handleEvent(data, next, main, 'OnLoadTransactionScope')});
         pepperi.events.intercept('IncrementFieldValue', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'IncrementFieldValue') });
         pepperi.events.intercept('DecrementFieldValue', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'DecrementFieldValue') });
         pepperi.events.intercept('SetFieldValue', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'SetFieldValue') });
+        pepperi.events.intercept('WFActionOded', {}, async (data, next, main) => { return await this.handleEvent(data, next, main, 'WFActionOded') });
+
+        // now we need to register to any workflow action events
+        // const drafts = await pepperi.addons.configurations.uuid(AddonUUID).schema(atdFlowsConfigurationSchemaName).get();
+        // if (drafts.length > 0) {
+        //     drafts.forEach(draft => {
+        //         const events = draft.Data.Events;
+        //         if (events) {
+        //             events.forEach(event => {
+        //                 if (event.EventKey.startsWith('WFAction')) {
+        //                     debugger;
+        //                     // pepperi.events.intercept(event.EventKey, {}, async (data, next, main) => {
+        //                     //     debugger;
+        //                     //     return await this.handleEvent(data, next, main, event.EventKey)
+        //                     // });
+        //                 }
+        //             });
+        //         }
+        //     });
+        // }
     }
 
     private async handleEvent(data, next, main, eventKey) {
@@ -19,6 +41,7 @@ export class SubscriptionService {
             NextFunction: next,
             MainFunction: main
         }
+        debugger;
         const emitter = EventEmitterFactory.create(eventKey, interceptorData);
         return emitter ? await emitter.handleEvent() : undefined;
     }
